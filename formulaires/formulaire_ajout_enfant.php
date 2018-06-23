cddc589a5475d5af27d009d1543c406490898206
audreybsnrd <39850754+audreybsnrd@users.noreply.github.com>
2018-06-23 13:15:38 +0200

    Update formulaire_ajout_enfant.php

--- formulaires/formulaire_ajout_enfant.php
+++ formulaires/formulaire_ajout_enfant.php
@@ -7,7 +7,10 @@ session_start() ;
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>AJOUTER UN ENFANT</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="formulaire_nounou.css" />
+    
+    <title>Vos enfants | Les Bambins</title>
 </head>
 <body>
 
@@ -26,24 +29,42 @@ session_start() ;
 
 <form name="ajout_enfant" method='post' action='traitement_ajout_enfant.php'>
 
-    <p>
-        <label for="prenom">Prénom :</label><input type="text" name="prenom" id="prenom" placeholder="Votre prénom..." required/>
-    </p>
-
-    <p>
-        <label for="ddn"> Date de naissance :</label><input type="date" name="ddn" id="ddn" required>
-    </p>
-
-    <p>
-        <textarea name="restriction" id="restriction" placeholder="Restrictions alimentaires de votre enfant " cols="60" rows="4" required></textarea>
-    </p>
-
-
-    <p>
-        <input type="submit" value="Envoyer" />
-    </p>
-
-
+    <div>    
+            <h2>
+                <label for="enfant">Enfant 1 :</label>
+            </h2>
+            
+            <p>
+                <label for="prenom">Prénom :</label><input type="text" name="prenom" id="prenom" placeholder="Son prénom..." required/>
+                <label for="ddn">Date de naissance :</label><input type="date" name="ddn" id="ddn" placeholder="Sa date de naissance..." required/>
+            </p>
+            
+            <p>
+                <label for="restriction">Restrictions alimentaires :</label>
+            </p>
+           
+            <p>
+                <textarea name="restriction" id="restriction" placeholder="Indiquez-nous ses éventuelles restrictions alimentaires..." rows="3" cols="80"></textarea>
+            </p>          
+            
+            <p><input type="button" onclick="ajout(this);" value="+"/></p>
+            
+            <p>
+                <input type="submit" value="Envoyer" />
+            </p>   
+        
+    </div>
 </form>
+    
+    <script type="text/Javascript>
+        fonction ajout(element){
+            var formulaire = document.ajout_enfant;
+               // On clone le bouton d'ajout
+            var ajout = element.cloneNode(true);
+               // Crée un nouvel élément de type "input"
+            var champ = document.createElement("input");
+        }
+    </script>
+    
 </body>
 </html>
