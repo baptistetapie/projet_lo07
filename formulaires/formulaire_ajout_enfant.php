9c61fad521095c3ffc1e250abd2558f4f2efa3d5
TAPIE <baptiste.tapie@utt.fr>
2018-06-25 22:42:01 +0200

    B

--- formulaires/formulaire_ajout_enfant.php
+++ formulaires/formulaire_ajout_enfant.php
@@ -8,7 +8,7 @@ session_start() ;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="formulaire_nounou.css" />
+    <link rel="stylesheet" href="../CSS/formulaire_nounou.css" />
     
     <title>Vos enfants | Les Bambins</title>
 </head>
